e309c3f70661630150d4b56f9246de987137b42f
Simon Rupp-Greene <siru4581@colorado.edu>
2015-03-03 01:58:53 +0000

    completed ch4 exercises

---
+++ person_classes.rb
@@ -0,0 +1,7 @@
+person1 = {first: "bob", last: "smith"}
+person2 = {first: "jane", last: "smith"}
+person3 = {first: "joey", last: "smith"}
+
+params = {father: person1, mother: person2, child: person3}
+
+puts params
